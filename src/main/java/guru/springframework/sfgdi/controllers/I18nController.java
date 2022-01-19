df4519cfb5fb38c6f62f89c5b7ef321e77a517fe
Tosh-PC <lextor78@gmail.com>
2022-01-19 20:06:23 +0600

    Spring Profiles

---
+++ src/main/java/guru/springframework/sfgdi/controllers/I18nController.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Alex Gutorov
+ * @version 1.0
+ * @created 19/01/2022 - 18:55
+ */
+@Controller
+public class I18nController {
+
+    private final GreetingService greetingService;
+
+    public I18nController(@Qualifier("i18nService") GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello() {
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Alex Gutorov
+ * @version 1.0
+ * @created 19/01/2022 - 19:00
+ */
+@Profile("ES")
+@Service("i18nService")
+public class I18nSpanishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hola Mundo - ES";
+    }
+}
